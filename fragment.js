7141b96c47b74c773223282777cd5f3721c3bea5
mlebrun <mlebrun@ALT015-M.local>
2013-04-03 18:31:01 +0200

    Updated gh-pages to updated version

--- fragment.js
+++ fragment.js
@@ -27,7 +27,7 @@ window.fragment = { render: null, html: 'fragment', json: 'fragment-json', jsonp
       xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
           callback(xhr.responseText);
-       }
+        }
       }
       xhr.send();
     }
@@ -35,46 +35,94 @@ window.fragment = { render: null, html: 'fragment', json: 'fragment-json', jsonp
     else {
       url += (parser.search == '' ? '?' : '&') + fragment.jsonp + '=JSONPCallback';
       var script = document.createElement('script');
+      var parent;
       script.src = url;
       JSONPCallback = function(d) {
         callback(JSON.stringify(d));
+        JSONPCallback = null;
+        parent = script.parentNode;
+        if(parent) {
+          parent.removeChild(script);
+        }
+        script = null;
       };
       document.getElementsByTagName('head')[0].appendChild(script);
     }
   };
 
-  var fragments = document.querySelectorAll('[data-'+fragment.html+'][data-'+fragment.json+']');
-  Array.prototype.forEach.call(fragments, function(element) {
-    var htmlUrl = element.getAttribute('data-fragment');
-    var jsonUrl = element.getAttribute('data-fragment-json');
+  var status = false; 
+  var stack = [];
 
-    load(htmlUrl, function(html) {
-      load(jsonUrl, function(json) {
-        element.innerHTML = window.fragment.render(html, JSON.parse(json));
+  function ready(fn){
+    if(typeof fn != 'function' || Object.prototype.toString.call(fn) != '[object Function]') {
+      return;
+    }
+    if(status) {
+      setTimeout(fn, 0);
+    } else {
+      stack.push(fn);
+    }
+  }
+
+  function updateStatus(){ 
+    if(!/in/.test(document.readyState) && document.body) {
+      status = true;
+      stack.forEach(function(fn){ 
+        setTimeout(fn, 0); 
+      })
+      stack = [];
+    }
+    if(!status) {
+      setTimeout(updateStatus, 10);
+    }
+  }
+
+  setTimeout(updateStatus, 10);
+
+  var each = [].forEach
+
+  function evaluate(scope){
+    if(!scope || !scope.querySelectorAll) {
+      scope = document;
+    }
+    var fragments = scope.querySelectorAll('[data-'+fragment.html+'][data-'+fragment.json+']');
+    each.call(fragments, function(element) {
+      var htmlUrl = element.getAttribute('data-fragment');
+      var jsonUrl = element.getAttribute('data-fragment-json');
+
+      load(htmlUrl, function(html) {
+        load(jsonUrl, function(json) {
+          element.innerHTML = window.fragment.render(html, JSON.parse(json));
+        });
       });
     });
-  });
 
-  var fragments = document.querySelectorAll('[data-'+fragment.html+']:not([data-'+fragment.json+'])');
-  Array.prototype.forEach.call(fragments, function(element) {
-    var htmlUrl = element.getAttribute('data-fragment');
+    fragments = scope.querySelectorAll('[data-'+fragment.html+']:not([data-'+fragment.json+'])');
+    each.call(fragments, function(element) {
+      var htmlUrl = element.getAttribute('data-fragment');
 
-    load(htmlUrl, function(html) {
-      if (element.innerHTML == '') {
-        element.innerHTML = html;
-      }
-      else {
-        element.innerHTML = window.fragment.render(html, JSON.parse(element.innerHTML));
-      }
+      load(htmlUrl, function(html) {
+        if (element.innerHTML == '') {
+          element.innerHTML = html;
+        }
+        else {
+          element.innerHTML = window.fragment.render(html, JSON.parse(element.innerHTML));
+        }
+      });
     });
-  });
 
-  var fragments = document.querySelectorAll('[data-'+fragment.json+']:not([data-'+fragment.html+'])');
-  Array.prototype.forEach.call(fragments, function(element) {
-    var jsonUrl = element.getAttribute('data-fragment-json');
+    fragments = scope.querySelectorAll('[data-'+fragment.json+']:not([data-'+fragment.html+'])');
+    each.call(fragments, function(element) {
+      var jsonUrl = element.getAttribute('data-fragment-json');
 
-    load(jsonUrl, function(json) {
-      element.innerHTML = window.fragment.render(element.innerHTML, JSON.parse(json));
+      load(jsonUrl, function(json) {
+        element.innerHTML = window.fragment.render(element.innerHTML, JSON.parse(json));
+      });
     });
-  });
+  }
+
+  fragment.evaluate = function(){
+    ready(evaluate);
+  }
+
 })(fragment);
